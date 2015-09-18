60eb4838e5f73d4a1b9e2854b3aaed1acf92fe83
Barthelemy Delemotte <barthelemy.delemotte@gmail.com>
2015-09-18 14:45:22 +0200

    prlimit done

--- prlimit.c
+++ prlimit.c
@@ -1,3 +1,19 @@
-int main(void){
+#include <limits.h>
+#include <unistd.h>
+#include <stdio.h>
+
+void prlimit(void){
+
+  printf("Longueur maximale d’un nom d’entrée dans le système de fichiers(NAME_MAX) : %d\n\
+Longueur maximale d’un chemin dans le système de fichiers (PATH_MAX) : %d\n", NAME_MAX, PATH_MAX);
+
+}
+
+
+int main(int argc, char const *argv[]) {
+
+  prlimit();
+
   return 0;
+
 }
